577cd691e0f9eadf0e3b2c2c576bca1f2ad69615
wwlouis00 <wwlouis00@gmail.com>
2023-07-14 02:41:00 +0800

    ultrakytics

---
+++ cpu/CPU_SeeON.py
@@ -0,0 +1,22 @@
+import os
+import datetime
+
+TestTime = datetime.datetime.now().strftime('%Y%m%d_%H%M')
+
+try:
+    path = os.path.abspath('.') + "\CPU_Usage_Test"
+    if not os.path.isdir(path):
+        os.mkdir(path)
+except :
+ print("Error! Could not create " + path)
+ 
+
+logname = path + r"\CPU_SeeON_"+ TestTime +".txt"
+print("[Path] " + logname + '\n')
+
+Cputest = r"adb shell cpu_usage >> " + logname
+
+os.system(Cputest)
+
+print("End")
+os.system("pause")
